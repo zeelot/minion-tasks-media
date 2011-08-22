22e9d3613ded8a806dfc8c6e86a53d0cc6978225
Lorenzo Pisani <zeelot3k@gmail.com>
2011-08-21 22:49:50 -0700

    adding stub js and css compilers

---
+++ classes/compiler/css.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Compiler_CSS {
+
+	public static function compile(array $filepaths)
+	{
+
+	}
+}
---
+++ classes/compiler/js.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Compiler_JS {
+
+	public static function compile(array $filepaths)
+	{
+
+	}
+}
